6d4515aeaf87ef268587dfa4151c87306cce33a1
navya1220 <navyakalla1111@gmail.com>
2024-04-13 21:12:31 +0530

    uploaded files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import Login from './components/Login'
@@ -9,13 +9,13 @@ import './App.css'
 const App = () => (
     <div className='Total-page'>
         <Header/>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/classes" component={Classes}/>
-            <Route exact path="/products" component={Products}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/login" component={Login}/>
-        </Switch>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="classes" element={<Classes/>}/>
+            <Route exact path="products" element={<Products/>}/>
+            <Route exact path="about" element={<About/>}/>
+            <Route exact path="login" element={<Login/>}/>
+        </Routes>
     </div>
     )
 
