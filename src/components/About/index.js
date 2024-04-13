5574a5f5bbf8c775ef31e67569ed26ca27180768
navya1220 <navyakalla1111@gmail.com>
2024-04-13 11:24:46 +0530

    uploaded files

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,32 @@
+import './index.css'
+
 const About = () =>  (
-    <div>
-        <h1>navya</h1>
+    <div className="about-container">
+        <div className="about-card">
+            <img
+                className="about-image"
+                src='https://cdn.arihantbooks.com/assets/ProductImage/Prod_1362_13012023184808.jpg'
+                alt="about-image"
+            />
+            <p className="about-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div className="about-card">
+             <p className="about-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <img
+                className="about-image"
+                src='https://cdn.arihantbooks.com/assets/ProductImage/Prod_1362_13012023184808.jpg'
+                alt="about-image"
+            />
+    
+        </div>
+        <div className="about-card">
+            <img
+                className="about-image"
+                src='https://cdn.arihantbooks.com/assets/ProductImage/Prod_1362_13012023184808.jpg'
+                alt="about-image"
+            />
+            <p className="about-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
     </div>
 )
 export default About
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,6 +1,49 @@
+import './index.css'
+
+const productList = [
+    {
+        id: 1,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYExByMB9Pmixm1K0yjOpILRCkSSJnHlmSIr1etU38FbHQde1_jeHrkHxCp4OKqFYQjfQ&usqp=CAU",
+        title : "Product Name",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        rupees : "1500/-"
+    },
+    {
+        id: 2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYExByMB9Pmixm1K0yjOpILRCkSSJnHlmSIr1etU38FbHQde1_jeHrkHxCp4OKqFYQjfQ&usqp=CAU",
+        title : "Product Name",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        rupees : "1500/-"
+    },
+    {
+        id: 3,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYExByMB9Pmixm1K0yjOpILRCkSSJnHlmSIr1etU38FbHQde1_jeHrkHxCp4OKqFYQjfQ&usqp=CAU",
+        title : "Product Name",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        rupees : "1500/-"
+    },
+    
+]
+
 const Products = () =>  (
     <div>
-        <h1>navya</h1>
+        <ul className="product-container">
+        {productList.map(eachProfile => (
+            <li className="product-card">
+                <img
+                    src = {eachProfile.image}
+                    alt = "product-image"
+                    className="product-image"
+                />
+                <div className="product-card-details">
+                    <h1 className="title-name">{eachProfile.title}</h1>
+                    <p className="title-description">{eachProfile.description}</p>
+                    <p className='product-rupees'>{eachProfile.rupees}</p>
+                    <button className="add-to-card-button" type="button">Add to Cart</button>
+                </div>
+            </li>
+        ))}
+        </ul>
     </div>
 )
 export default Products
