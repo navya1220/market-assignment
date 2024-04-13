5b59a7c1222ed875ebf55e19d011ded0fd344be4
navya1220 <navyakalla1111@gmail.com>
2024-04-13 09:46:23 +0530

    uploaded files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import {Route, Switch} from 'react-router-dom'
+
 import Header from './components/Header'
 import Home from './components/Home'
-import Login from './components/Login'
-import About from './components/About'
-import Classes from './components/Classes'
-import Products from './components/Products'
+
 import './App.css'
 const App = () => (
     <div className='Total-page'>
         <Header/>
-        <div className='app-body'>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/classes" component={Classes}/>
-            <Route exact path="/products" component={Products}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/login" component={Login}/>
-        </Switch>
-        </div>
+        <Home/>
     </div>
     )
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,9 +8,7 @@ const Header = () => (
         </div>
         <ul className='nav-items-list'>
             <li className='link-item'>
-                <Link to="/">
                 <button className='button'>JOIN US</button>
-                </Link>
             </li>
             <li className='link-item'>
                 <Link to="/classes">
